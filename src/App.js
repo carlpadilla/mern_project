24eec0b0d73bc17fa9b7dce079f5ef3045c3ea53
carlpadilla <carlpadilla@protonmail.com>
2020-08-26 12:41:02 -0400

    added  routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Mern Project</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   );
 }
 
