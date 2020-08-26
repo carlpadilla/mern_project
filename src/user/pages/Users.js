bca5b75aec86450f18092f293ac0038246a78715
carlpadilla <carlpadilla@protonmail.com>
2020-08-26 13:45:57 -0400

    added more styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -11,6 +11,13 @@ function Users() {
         'https://image.shutterstock.com/image-vector/question-mark-on-head-vector-600w-1089659894.jpg',
       places: 3,
     },
+    {
+      id: 'u2',
+      name: 'Jenny Padilla',
+      image:
+        'https://image.shutterstock.com/image-vector/question-mark-on-head-vector-600w-1089659894.jpg',
+      places: 1,
+    },
   ];
 
   return <UsersList items={USERS} />;
