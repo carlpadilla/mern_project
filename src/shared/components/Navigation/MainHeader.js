48a34328d3daef34f19f7758aef9b9e7893baee8
carlpadilla <carlpadilla@protonmail.com>
2020-08-28 21:14:58 -0400

    starting header

---
+++ src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainHeader() {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default MainHeader
+<h1>Header</h1>
