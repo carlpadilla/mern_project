a3bdd5e6878268758922632961135a1edae0105c
carlpadilla <carlpadilla@protonmail.com>
2020-09-19 04:22:12 -0400

    routing params;

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
@@ -32,7 +33,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPLaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPLaces} />;
 };
 
 export default UserPlaces;
