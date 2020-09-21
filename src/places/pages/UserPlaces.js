074fe543c5b297d664998a846fcb5f2aa84c44c8
Carl Padilla <carlpadilla@protonmail.com>
2020-09-21 14:36:11 -0400

    added google maps api

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -13,7 +13,7 @@ const DUMMY_PLACES = [
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lon: -73.9878531,
+      lng: -73.9878531,
     },
     creator: 'u1',
   },
@@ -26,7 +26,7 @@ const DUMMY_PLACES = [
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lon: -73.9878531,
+      lng: -73.9878531,
     },
     creator: 'u2',
   },
