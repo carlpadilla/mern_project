4acd15712ea8be189251853a4e41d9547ab34511
Carl Padilla <carlpadilla@protonmail.com>
2020-11-04 14:22:24 -0500

    added comments

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes); // => /api/places/...
-app.use('/api/users', usersRoutes);
+app.use('/api/users', usersRoutes); // => /api/user/...
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route', 404);
@@ -27,6 +27,7 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error has occurred.' });
 });
 
+// Database connection.
 mongoose
   .connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
