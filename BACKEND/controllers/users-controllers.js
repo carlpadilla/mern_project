b356889976bae8706e5cb6940dd9099612eb2be1
Carl Padilla <carlpadilla@protonmail.com>
2020-11-16 12:22:46 -0500

    login code updated and tested

--- BACKEND/controllers/users-controllers.js
+++ BACKEND/controllers/users-controllers.js
@@ -63,18 +63,21 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError('Login failed, please try again later.', 500);
+    return next(error);
+  }
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    return next(
-      new HttpError(
-        'Could not identify user, possible incorrect credentials',
-        401
-      )
-    );
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError('Invalid credentials.', 401);
+
+    return next(error);
   }
 
   res.json({ message: 'logged in' });
